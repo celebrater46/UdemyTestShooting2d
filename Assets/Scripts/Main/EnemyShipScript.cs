ca6503a9b46f8585eb352d0c52918e6c0740ea66
hideru <jota.16.stigma.world@gmail.com>
2021-07-13 22:11:25 +0900

    L43

--- Assets/Scripts/Main/EnemyShipScript.cs
+++ Assets/Scripts/Main/EnemyShipScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,5 +15,13 @@ public class EnemyShipScript : MonoBehaviour
     void Update()
     {
         transform.position -= new Vector3(0, 2 * Time.deltaTime, 0);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("You got him!!");
+        Destroy(this.gameObject);
+        Destroy(other.gameObject);
     }
 }
