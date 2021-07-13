673452db74893a30f05683877cfe4dafea95acd4
hideru <jota.16.stigma.world@gmail.com>
2021-07-13 21:18:06 +0900

    L39

---
+++ Assets/Scripts/Main/BackgroundManagerScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -0.002f, 0);
+        if (transform.position.y < -10)
+        {
+            transform.position = new Vector3(0, 10, 0);
+        }
+    }
+}
