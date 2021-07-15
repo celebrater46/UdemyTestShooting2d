0ad607bd44d463bf734b184f0d4fce7706c53a87
hideru <jota.16.stigma.world@gmail.com>
2021-07-16 01:15:47 +0900

    L51 (ShotN())

---
+++ Assets/Scripts/Main/BossBulletScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBulletScript : MonoBehaviour
+{
+    private float x;
+    private float y;
+
+    public void Setting(float angle, float speed)
+    {
+        // x = 3;
+        // y = -3;  
+
+        // V Fire to rightward
+        // x = Mathf.Cos(0);
+        // y = Mathf.Sin(0);
+        
+        // 2PI = 360 degrees
+        // PI = 180 degrees
+        // V Fire letfward
+        // x = Mathf.Cos(Mathf.PI);
+        // y = Mathf.Sin(Mathf.PI);
+        
+        // PI / 2 = 90 degrees
+        // V Fire upward
+        x = Mathf.Cos(Mathf.PI / 2); // 90
+        y = Mathf.Sin(Mathf.PI / 2); // 90
+        
+        // PI / 2 = 90 degrees
+        // V Fire downward
+        // x = Mathf.Cos(-Mathf.PI / 2); // -90
+        // y = Mathf.Sin(-Mathf.PI / 2); // -90
+        
+        x = Mathf.Cos(angle) * speed;
+        y = Mathf.Sin(angle) * speed;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.position -= new Vector3(0, 6, 0) * Time.deltaTime;
+        transform.position += new Vector3(x, y, 0) * Time.deltaTime;
+    }
+}
--- Assets/Scripts/Main/BossScript.cs
+++ Assets/Scripts/Main/BossScript.cs
@@ -6,13 +6,32 @@ public class BossScript : MonoBehaviour
 {
     public GameObject explosionPrefab;
     private GameManagerScriptMain gameManagerScriptMain;
-    public GameObject enemyBulletPrefab;
+    // public GameObject bossBulletPrefab;
+    public BossBulletScript bossBulletPrefab;
     
     // Start is called before the first frame update
     void Start()
     {
+        // Instantiate(bossBulletPrefab, transform.position, transform.rotation);
         gameManagerScriptMain = GameObject.Find("GameManager").GetComponent<GameManagerScriptMain>();
-        Shot();
+        // Shot();
+        // Shot(Mathf.PI / 4f); // 45 degrees (right upward)
+        // Shot(-Mathf.PI / 4f); // 45 degrees (right downward)
+        // Shot(-Mathf.PI / 2f); // 45 degrees (downward)
+        
+        // 2PI = 360 degrees
+        // PI  = 180 degrees
+        
+        // Right == 0PI(2PI)
+        // UP == 0.5PI
+        // Left == 1PI
+        // Down == 1.5PI
+        // Shot(Mathf.PI * 1.75f); // 45 degrees (left downward)
+        // Shot(Mathf.PI * 1.5f); // 45 degrees (downward)
+        // Shot(Mathf.PI * 1.25f); // 45 degrees (right downward)
+
+        ShotN(10, 4f);
+
     }
 
     // Update is called once per frame
@@ -23,6 +42,10 @@ public class BossScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.CompareTag("EnemyBullet"))
+        {
+            return;
+        }
         Instantiate(explosionPrefab, transform.position, transform.rotation);
         if (other.CompareTag("Bullet"))
         {
@@ -37,8 +60,19 @@ public class BossScript : MonoBehaviour
         Destroy(gameObject);
     }
 
-    private void Shot()
+    private void Shot(float angle, float speed)
     {
-        Instantiate(enemyBulletPrefab, transform.position, transform.rotation);
+        BossBulletScript bullet = Instantiate(bossBulletPrefab, transform.position, transform.rotation);
+        bullet.Setting(angle, speed);
+    }
+
+    private void ShotN(int count, float speed)
+    {
+        // int count = 8;
+        for (int i = 0; i < count; i++)
+        {
+            float angle = Mathf.PI / count * 2 * i;
+            Shot(angle, speed);
+        }
     }
 }
